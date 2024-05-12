1ab1b9dd9b209690e3bf00d9c4ec99ccd0c5eb5d
Fabian Mettler <dev@maveonair.com>
2024-05-12 08:41:09 +0200

    Fix for Firefox to prevent the cursor from jumping to the beginning of the token after entering a character

--- src/components/Encoded.tsx
+++ src/components/Encoded.tsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
+
 type EncodedProps = {
   token: string | null;
   setToken: (token: string | null) => void;
 };
 
 export default function Encoded({ token, setToken }: EncodedProps) {
+  const [placeholder, setPlaceholder] = useState<string | null>();
+
+  useEffect(() => {
+    setPlaceholder(token);
+  }, []);
+
   return (
     <div className="h-full">
       <h1 className="text-2xl mb-3 dark:text-white">Encoded</h1>
@@ -11,12 +19,12 @@ export default function Encoded({ token, setToken }: EncodedProps) {
         <div
           aria-label="token-input"
           aria-placeholder="Enter your token here"
-          className="w-full h-full p-4 border rounded-md text-sm font-mono focus:outline focus:outline-2 focus:outline-indigo-600 dark:bg-dark-dimmed dark:text-white dark:border-gray-800"
+          className="w-full h-full p-4 border rounded-md text-sm text-wrap font-mono focus:outline focus:outline-2 focus:outline-indigo-600 dark:bg-dark-dimmed dark:text-white dark:border-gray-800"
           contentEditable
           onInput={(e) => setToken(e.currentTarget.textContent)}
           suppressContentEditableWarning={true}
         >
-          {token}
+          {placeholder || token}
         </div>
       </div>
     </div>
